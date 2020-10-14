29a57c387ae3f25ecb7d7ee6149ba690a683a751
Malcolm Sparks <mal@juxt.pro>
2020-10-14 17:02:06 +0100

    Rewrite resource tests in terms of the handler API
    Testing at the level of an implementation function risks not testing the
    wrapping middleware.

--- test/juxt/spin/resource_test.clj
+++ test/juxt/spin/resource_test.clj
@@ -4,14 +4,12 @@
   (:require
    [clojure.test :refer [deftest is use-fixtures testing]]
    [juxt.spin.alpha.methods :refer [http-method]]
+   [juxt.spin.alpha.handler :refer [handler]]
    [juxt.spin.alpha.resource :as r]
    [ring.mock.request :refer [request]]
-   [clojure.tools.logging :as log]
-   [juxt.spin.alpha.methods :as methods])
+   [clojure.tools.logging :as log])
   (:import
-   (java.util.logging LogManager Logger Level Handler)))
-
-;;(remove-ns (symbol (str *ns*)))
+   (java.util.logging Logger Level Handler)))
 
 (def ^:dynamic  *log-records* [])
 
@@ -34,14 +32,11 @@
 (use-fixtures :each with-log-capture)
 
 (deftest get-with-body-default-status-test
-  (http-method
-   (reify
-     r/GET
-     (get-or-head [_ server resource response request respond raise]
-       (respond (conj response [:body "Hello World!"]))))
-   nil                                ; nil server-provider
-   {}                                ; resource
-   {}                                 ; response
+  ((handler
+    (reify
+      r/GET
+      (get-or-head [_ server resource response request respond raise]
+       (respond (conj response [:body "Hello World!"])))))
    (request :get "/")
    (fn [response]
      (is (= 200 (:status response)))
@@ -49,15 +44,15 @@
    (fn [t] (throw t))))
 
 (deftest get-with-body-explicit-status-test
-  (http-method
-   (reify
-     r/GET
-     (get-or-head [_ server resource response request respond raise]
-       (respond (merge response {:status 200
-                                 :body "Hello World!"}))))
-   nil                                ; nil server-provider
-   nil                                ; nil resource
-   {}                                 ; response
+  ((handler
+    (reify
+      r/GET
+      (get-or-head [_ server resource response request respond raise]
+        (respond
+         (merge
+          response
+          {:status 200
+           :body "Hello World!"})))))
    (request :get "/")
    (fn [response]
      (is (= 200 (:status response)))
@@ -65,15 +60,15 @@
    (fn [t] (throw t))))
 
 (deftest get-with-body-explicit-not-ok-test
-  (http-method
-   (reify
-     r/GET
-     (get-or-head [_ server resource response request respond raise]
-       (respond (merge response {:status 400
-                                 :body "Bad request!"}))))
-   nil                                ; nil server-provider
-   nil                                ; nil resource
-   {}                                 ; response
+  ((handler
+    (reify
+      r/GET
+      (get-or-head [_ server resource response request respond raise]
+        (respond
+         (merge
+          response
+          {:status 400
+           :body "Bad request!"})))))
    (request :get "/")
    (fn [response]
      (is (= 400 (:status response)))
@@ -81,17 +76,14 @@
    (fn [t] (throw t))))
 
 (deftest get-with-content-response
-  (http-method
-   (reify
-     r/ContentResponse
-     (respond-with-content [_ server resource representations response request respond raise]
-       (respond
-        (-> response
-            (update :headers (fnil conj {}) ["content-type" "text/plain;charset=utf8"])
-            (conj [:body "Hello World!"])))))
-   nil                                ; nil server-provider
-   {}                                 ; resource
-   {}                                 ; response
+  ((handler
+    (reify
+      r/ContentResponse
+      (respond-with-content [_ server resource representations response request respond raise]
+        (respond
+         (-> response
+             (update :headers (fnil conj {}) ["content-type" "text/plain;charset=utf8"])
+             (conj [:body "Hello World!"]))))))
    (request :get "/")
    (fn [response]
      (is (= 200 (:status response)))
@@ -100,77 +92,71 @@
    (fn [t] (throw t))))
 
 (deftest get-with-no-representation-404-test
-  (http-method
-   (reify
-     r/ContentVariants
-     (available-variants [_ server resource response]
-       (case (:status response)
-         200 []
-         404 [{:juxt.http/content-type "text/plain;charset=utf8"}])))
-   nil                                  ; nil server-provider
-   nil                                  ; nil resource
-   {}                                   ; response
+  ((handler
+    (reify
+      r/ResourceLocator
+      (locate-resource [_ uri request]
+        nil)
+      r/ContentVariants
+      (available-variants [_ server resource response]
+        (case (:status response)
+          200 []
+          404 [{:juxt.http/content-type "text/plain;charset=utf8"}]))))
    (request :get "/")
    (fn [response]
      (is (= 404 (:status response))))
    (fn [t] (throw t))))
 
 (deftest get-with-no-acceptable-representation-406-test
-  (http-method
-   (reify
-     r/ContentVariants
-     (available-variants [_ server resource response]
-       (case (:status response)
-         200 [:json]
-         ;; Here's our error message!
-         406 [{:juxt.http/content-type "text/plain;charset=utf8"}]
-         (throw (ex-info "Unexpected case" {:response response}))))
+  ((handler
+    (reify
+      r/ContentVariants
+      (available-variants [_ server resource response]
+        (case (:status response)
+          200 [:json]
+          ;; Here's our error message!
+          406 [{:juxt.http/content-type "text/plain;charset=utf8"}]
+          (throw (ex-info "Unexpected case" {:response response}))))
 
-     r/ContentProactiveNegotiation
-     (select-representations [_ server request variants]
-       (remove #(= :json %) variants)))
-   nil                                ; nil server-provider
-   {}                                 ; nil resource
-   {}                                 ; response
+      r/ContentProactiveNegotiation
+      (select-representations [_ server request variants]
+        (remove #(= :json %) variants))))
    (request :get "/")
    (fn [response]
      (is (= 406 (:status response))))
    (fn [t] (throw t))))
 
 (deftest get-with-varying-content-type-test
-  (let [rp (reify
-             r/ContentVariants
-             (available-variants [_ server resource response]
-               [{:juxt.http/content-type "text/html;charset=utf8"}
-                {:juxt.http/content-type "application/json"}])
+  (let [handler
+        (handler
+         (reify
+           r/ContentVariants
+           (available-variants [_ server resource response]
+             [{:juxt.http/content-type "text/html;charset=utf8"}
+              {:juxt.http/content-type "application/json"}])
 
-             r/ContentProactiveNegotiation
-             (select-representations [_ server request variants]
-               (is (= 2 (count variants)))
-               (is (= [{:juxt.http/content-type "text/html;charset=utf8"}
-                       {:juxt.http/content-type "application/json"}]
-                      variants))
-               ;; In the absence of a proactive negotiation algorithm, just pick the
-               ;; first (the text/html variant).
-               (take 1 variants))
+           r/ContentProactiveNegotiation
+           (select-representations [_ server request variants]
+             (is (= 2 (count variants)))
+             (is (= [{:juxt.http/content-type "text/html;charset=utf8"}
+                     {:juxt.http/content-type "application/json"}]
+                    variants))
+             ;; In the absence of a proactive negotiation algorithm, just pick the
+             ;; first (the text/html variant).
+             (take 1 variants))
 
-             r/ContentResponse
-             (respond-with-content [_ server resource representations response request respond raise]
-               (log/trace "response is" (pr-str response))
-               (is (= 1 (count representations)))
-               (is (= {:juxt.http/content-type "text/html;charset=utf8"} (first representations)))
-               (respond
-                (-> response
-                    (update :headers (fnil conj {}) ["content-type" (:juxt.http/content-type (first representations))])
-                    (conj [:body (case (:juxt.http/content-type (first representations))
-                                   "text/html;charset=utf8" "<h1>Hello World!</h1>"
-                                   "application/json" "{\"message\": \"Hello World!\"}")])))))]
+           r/ContentResponse
+           (respond-with-content [_ server resource representations response request respond raise]
+             (is (= 1 (count representations)))
+             (is (= {:juxt.http/content-type "text/html;charset=utf8"} (first representations)))
+             (respond
+              (-> response
+                  (update :headers (fnil conj {}) ["content-type" (:juxt.http/content-type (first representations))])
+                  (conj [:body (case (:juxt.http/content-type (first representations))
+                                 "text/html;charset=utf8" "<h1>Hello World!</h1>"
+                                 "application/json" "{\"message\": \"Hello World!\"}")]))))))]
     (testing "GET"
-      (http-method
-       rp
-       nil                              ; nil server-provider
-       {}                               ; resource
-       {}                               ; response
+      (handler
        (request :get "/")
        (fn [response]
          (is (= 200 (:status response)))
@@ -180,11 +166,7 @@
        (fn [t] (throw t))))
 
     (testing "HEAD"
-      (http-method
-       rp
-       nil                            ; nil server-provider
-       {}                             ; resource
-       {}                             ; response
+      (handler
        (request :head "/")
        (fn [response]
          (is (= 200 (:status response)))
