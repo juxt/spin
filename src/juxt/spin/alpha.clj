7931f497e6a254d274860a48c25448991ce35784
Malcolm Sparks <mal@juxt.pro>
2020-11-27 16:31:32 +0000

    Remove clojure.spec
    The single fdef and instrument is gone, so these declarations don't add value
    over the README. They may be reinstated when Spin is more stable.

--- src/juxt/spin/alpha.clj
+++ src/juxt/spin/alpha.clj
@@ -3,24 +3,9 @@
 (ns juxt.spin.alpha
   (:require
    [clojure.string :as str]
-   [clojure.spec.alpha :as s]
    [juxt.spin.alpha.util :as util])
   (:import
    (java.util Date)))
-
-(s/def :ring.request/method keyword?)
-(s/def :ring.request/path string?)
-(s/def :ring.request/headers map?)
-
-(s/def ::request
-  (s/keys :req [:ring.request/method
-                :ring.request/path]
-          :opt [:ring.request/headers]))
-
-(s/def ::ctx (s/keys :opt [::request ::raise!]))
-
-(s/def ::resource
-  (s/keys :opt []))
 
 (defn sync-adapt [h]
   (fn this
