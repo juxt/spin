581f3af4e79fd817a35493c703c32cbb876077fa
Malcolm Sparks <mal@juxt.pro>
2020-12-04 16:34:31 +0000

    Reformat code to reduce width in README

--- src/juxt/spin/alpha.clj
+++ src/juxt/spin/alpha.clj
@@ -45,8 +45,11 @@
 (defn unknown-method?
   "When the request method is unknown, return a 501 response."
   [request]
-  (when-not (contains? #{:get :put :post :delete :options :trace :connect} (:request-method request))
-    {:status 501}))
+  (when-not
+      (contains?
+       #{:get :put :post :delete :options :trace :connect}
+       (:request-method request))
+      {:status 501}))
 
 (defn ok []
   {:status 200})
